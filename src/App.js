5632874a50cfef6d5b2db840d572ac6062ccd4bf
Zaw Zaw Myat Nyein <zawzawmyatnyein@gmail.com>
2023-03-07 19:26:07 +0630

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { BoltIcon } from '@heroicons/react/24/solid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center text-3xl text-violet-600">Hello World</h1>
+      <BoltIcon className="h-20 w-20 text-red-500" />
     </div>
   );
 }
